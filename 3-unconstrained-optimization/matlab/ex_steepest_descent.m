78ce8ae7622508b306ed134a27b778ee05956d7e
Daniel R. Herber <daniel.herber@colostate.edu>
2025-10-18 20:07:37 -0600

    Updates to ex_steepest_descent.m

--- 3-unconstrained-optimization/matlab/ex_steepest_descent.m
+++ 3-unconstrained-optimization/matlab/ex_steepest_descent.m
@@ -4,9 +4,9 @@
 % [course] Session 8 - Unconstrained Optimization (2)
 close all; clear; clc
 
-test = 2;
+example = 4;
 
-switch test
+switch example
     %----------------------------------------------------------------------
     case 1 % Example 12.1 in LNO
         Q = diag([1 5 25]);
@@ -20,7 +20,7 @@ switch test
         Q = create_rand_pd_matrix(d,s);
         c = [-1;-1];
         x0 = [0;0];
-        n = 20;
+        n = 25;
     %----------------------------------------------------------------------
     case 3 % cond(Q) is smallish
         d = [1 2];
@@ -40,27 +40,20 @@ end
 % optimal solution
 x_opt = Q\c;
 
-% check if this is a 2d problem for plotting
+% check if 2D problem that can be plotted
 if length(Q) == 2
     n2flag = true;
 else
     n2flag = false;
 end
 
-% setup figure
-if n2flag
-    set(0,'defaultTextInterpreter','latex');
-    hf = figure; hf.Color = 'w'; hold on
-    xlabel('$x$'); ylabel('$y$'); % label axes
-    axis equal
-    ha = gca;
-    ha.FontSize = 18;
-end
+% initialize figure (2D problem only)
+plot_2d([],[],[],[],[],[],n2flag,'init')
 
 % assign initial point
 x = x0;
 
-% initialize
+% initialize function value storage
 f = nan(1,n);
 
 % go through each iteration
@@ -70,7 +63,7 @@ for k = 1:n
     xold = x;
 
     % compute current function value
-    if k < 20
+    if k < 25
         f(k) = x'*Q*x/2 - c'*x;
     end
 
@@ -87,55 +80,13 @@ for k = 1:n
     % next point
     x = x + alpha*p;
 
-    % plot if it is a two dimensional problem
-    if n2flag
-
-        % plot line between old and new point
-        hp = plot([xold(1) x(1)],[xold(2) x(2)],'-b');
-        hp.LineWidth = 2;
-
-        % plot current point
-        hp = plot(xold(1),xold(2),'.r');
-        hp.MarkerSize = 18;
-
-    end
+    % plot iteration (2D problem only)
+    plot_2d(x,xold,x_opt,f,Q,c,n2flag,'iter')
 
 end
 
-% plot if it is a two dimensional problem
-if n2flag
-
-    % create a good set of contour lines
-    f(isnan(f)) = [];
-    fmax = 15;
-    f_ = linspace(max(f),fmax,30);
-    f = unique(sort([f,f_]));
-
-    % create grid
-    N = 1000;
-    x = linspace(-1.5,0.5,N);
-    y = linspace(-1,0.5,N);
-    [X,Y] = meshgrid(x,y);
-
-    % evaluate quadratic function
-    C = zeros(size(X));
-    for k = 1:numel(X)
-        x = [X(k);Y(k)];
-        C(k) = x'*Q*x/2 - c'*x;
-    end
-
-    % create contour plot
-    [~,hc] = contour(X,Y,C,f);
-    hc.LineWidth = 1;
-
-    % plot optimal point
-    hp = plot(x_opt(1),x_opt(2),'.g');
-    hp.MarkerSize = 18;
-
-    % axis equal to visualize condition number
-    axis equal
-
-end
+% plot contours and optimal point (2D problem only)
+plot_2d(x,xold,x_opt,f,Q,c,n2flag,'final')
 
 %--------------------------------------------------------------------------
 % create a random positive-definite matrix with given eigenvalues
@@ -154,5 +105,87 @@ n = length(d);
 
 % apply the transformation
 A = Q * diag(d) * transpose(Q);
+
+end
+
+%--------------------------------------------------------------------------
+% plotting functions
+% (not the main content)
+function plot_2d(x,xold,x_opt,f,Q,c,n2flag,tasknum)
+
+% only do stuff if a 2D problem
+if ~n2flag
+    return
+end
+
+% colors and other parameters
+niceblue = [77, 121, 167]/255;
+nicered = [225, 86, 86]/255;
+nicegreen = [109, 195, 80]/255;
+LineWidth = 1.5;
+MarkerSize = 24;
+FontSize = 12;
+plotOpts = {'LineWidth',LineWidth,'MarkerSize',MarkerSize};
+
+switch tasknum
+    %----------------------------------------------------------------------
+    case 'init' % initial point
+
+    % initialize figure
+    hf = figure; hf.Color = 'w'; hold on
+
+    % labels
+    xlabel('$x$','Interpreter','latex');
+    ylabel('$y$','Interpreter','latex');
+
+    % axis properties
+    ha = gca; ha.XColor = 'k'; ha.YColor = 'k'; ha.Color = 'none';
+    ha.LineWidth = 1; ha.FontSize = FontSize;
+    axis equal
+
+    %----------------------------------------------------------------------
+    case 'iter'
+
+    % plot line between old and new point
+    plot([xold(1) x(1)],[xold(2) x(2)],'-',plotOpts{:},'Color',niceblue);
+
+
+    % plot current point
+    plot(xold(1),xold(2),'.',plotOpts{:},'Color',nicered);
+
+    %----------------------------------------------------------------------
+    case 'final'
+
+    % create a good set of contour lines
+    f(isnan(f)) = [];
+    fmax = 15;
+    f_ = linspace(max(f),fmax,40);
+    f = unique(sort([f,f_]));
+
+    % create grid
+    N = 1000;
+    x = linspace(-1.5,0.5,N);
+    y = linspace(-1,0.5,N);
+    [X,Y] = meshgrid(x,y);
+
+    % evaluate quadratic function
+    C = zeros(size(X));
+    for k = 1:numel(X)
+        x = [X(k);Y(k)];
+        C(k) = x'*Q*x/2 - c'*x;
+    end
+
+    % create contour plot
+    [~,hc] = contour(X,Y,C,f);
+    hc.LineWidth = LineWidth;
+    colormap(bone)
+
+    % plot optimal point
+    plot(x_opt(1),x_opt(2),'.',plotOpts{:},'Color',nicegreen);
+
+    % axis equal to visualize condition number
+    axis equal
+
+end
 
 end
